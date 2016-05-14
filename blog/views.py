e87096b2bd6de63365642447ebd3a3053684a55f
Minerva Rubalcava <minervaris@gmail.com>
2016-05-14 14:34:15 -0600

    Change the HTML for the site

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,10 @@
 from django.shortcuts import render
 
 # Create your views here.
+# kasñldkasñd
+# kñalsdñlkasd
+# akdasñl
+# 
+
+def post_list(request):
+    return render(request, 'blog/post_list.html', {})
